2f89cd00b04dd8e0a55df546d759a242842a523e
Michal Mocny <mmocny@gmail.com>
2022-11-18 11:34:11 -0500

    brand new create-next-app without errors

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,19 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import delay from '../../utils/delay';
 
 type Data = {
-  name?: string,
-  now: string,
+  name: string
 }
 
-export default async function handler(
+export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log('API HIT Start');
-  await delay(1000);
-  console.log('API HIT End');
-
-  res.status(200).json({ now: (new Date).toISOString() });
+  res.status(200).json({ name: 'John Doe' })
 }
--- utils/getData.ts
+++
@@ -1,6 +0,0 @@
-export default async function getData() {
-	let result = await fetch('http://localhost:3000/api/hello', {
-		cache: 'no-store'
-	});
-	return await result.json();
-}
